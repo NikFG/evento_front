640a9c2ea37d20e9daf791216f15a425a0a148c0
Nikollas <nikollasferreirag@gmail.com>
2021-08-20 12:33:16 -0300

    feat: cria componente para os eventos

---
+++ pages/eventos/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@components/Navbar";
+import GridEventos from "@components/GridEventos";
+
+export default function Eventos() {
+
+    return (
+        <>
+            <Navbar/>
+            <GridEventos/>
+        </>
+    );
+}
