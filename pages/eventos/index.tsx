d64d6311148cb1e5e0a87675214c9c89de88f779
Nikollas <nikollasferreirag@gmail.com>
2021-08-30 14:54:35 -0300

    feat: cria pagina de eventos com dados do servidor

--- pages/eventos/index.tsx
+++ pages/eventos/index.tsx
@@ -1,12 +1,28 @@
 import Navbar from "@components/Navbar";
 import GridEventos from "@components/GridEventos";
+import {GetStaticProps, InferGetStaticPropsType} from "next";
+import axios from "axios";
+import {Categoria, Evento} from "@types";
 
-export default function Eventos() {
+export const getStaticProps: GetStaticProps = async (context) => {
+    const axios = require('axios');
+    const res = await axios.get('http://localhost:8000/api/eventos');
+
+    const eventos: Evento[] = res.data;
+
+    return {
+        props: {
+            eventos,
+
+        },
+    }
+}
+export default function Eventos({eventos}: InferGetStaticPropsType<typeof getStaticProps>) {
 
     return (
         <>
             <Navbar/>
-            <GridEventos/>
+            <GridEventos eventos={eventos}/>
         </>
     );
 }
