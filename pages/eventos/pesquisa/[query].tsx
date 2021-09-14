772c88f09c13c9beea40e4a7aa80bb98fd0e504f
Nikollas <nikollasferreirag@gmail.com>
2021-09-14 17:03:08 -0300

    feat: cria rota para recuperar os eventos do banco de dados
    Isto ocorre com uma query que é passada na caixa de pesquisa e resgatada na url

---
+++ pages/eventos/pesquisa/[query].tsx
@@ -0,0 +1,41 @@
+import {
+    GetServerSideProps,
+    GetStaticPaths, GetStaticProps, InferGetServerSidePropsType,
+    InferGetStaticPropsType,
+
+} from "next";
+import Navbar from "@components/Navbar";
+import {Evento} from "@types";
+import {ParsedUrlQuery} from "querystring";
+import GridEventos from "@components/GridEventos";
+
+
+interface IParams extends ParsedUrlQuery {
+    query: string
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const axios = require('axios')
+    const api = process.env.API_SERVER
+    const {query} = context.params as IParams
+    const url = api + "/eventos/pesquisa/" + encodeURIComponent(query.trim())
+    console.log(url)
+    const res = await axios.get(url);
+    const eventos: Evento[] = res.data;
+
+    return {
+        props: {
+            eventos
+        }
+    }
+}
+
+export default function PesquisaEventoPage({eventos}: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+    return (
+        <>
+            <Navbar/>
+            <GridEventos eventos={eventos}/>
+        </>
+    );
+}
