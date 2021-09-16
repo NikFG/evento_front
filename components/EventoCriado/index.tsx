952e16a346d950e35f2348a879992ec85c1868cc
Nikollas <nikollasferreirag@gmail.com>
2021-09-16 16:02:22 -0300

    feat: atualiza botão para levar a rota de atualizados

--- components/EventoCriado/index.tsx
+++ components/EventoCriado/index.tsx
@@ -3,16 +3,18 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit, faTrash, faArrowRight, faArrowDown} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import {Evento} from "@types";
+import {useRouter} from "next/router";
 
 export interface Props {
     evento: Evento
 }
 
-export default function EventoCriado(props: Props) {
+export default function EventoCriado({evento}: Props) {
+    const router = useRouter();
     const [expandido, setExpandido] = React.useState(false);
 
     function handleEdit(id: number | undefined) {
-
+        router.push(`/eventos/editar/${id}`)
     }
 
     function handleDelete(id: number | undefined) {
@@ -21,41 +23,41 @@ export default function EventoCriado(props: Props) {
 
     return (
         <>
-            <div key={props.evento.id} className={"row my-3"}>
+            <div key={evento.id} className={"row my-3"}>
                 <div className={"col-md-8 my-2"}>
 
                     <a className="" type="button"
                        data-bs-toggle="collapse"
-                       data-bs-target={"#eventoCollapse" + props.evento.id}
+                       data-bs-target={"#eventoCollapse" + evento.id}
                        aria-expanded="false"
                        onClick={() => {
                            setExpandido(!expandido);
                        }
                        }
-                       aria-controls={"eventoCollapse" + props.evento.id}>
+                       aria-controls={"eventoCollapse" + evento.id}>
                                         <span className={"mx-2"}>
                                             {!expandido ? <FontAwesomeIcon icon={faArrowRight}/> :
                                                 <FontAwesomeIcon icon={faArrowDown}/>}
                                         </span>
-                        {props.evento.nome}
+                        {evento.nome}
                     </a>
                 </div>
                 <div className={"col-md-4"}>
                     <button className={"btn btn-outline-secondary mx-2"} onClick={() => {
-                        handleEdit(props.evento.id)
+                        handleEdit(evento.id)
                     }}><FontAwesomeIcon icon={faEdit}/></button>
                     <button className={"btn btn-outline-danger mx-2"}><FontAwesomeIcon icon={faTrash} onClick={() => {
-                        handleDelete(props.evento.id)
+                        handleDelete(evento.id)
                     }}
                     /></button>
                 </div>
 
 
                 <div className="collapse multi-collapse mt-3"
-                     id={"eventoCollapse" + props.evento.id}>
+                     id={"eventoCollapse" + evento.id}>
                     <div className="card card-body">
-                        {props.evento.atividades.length > 0 ?
-                            props.evento.atividades.map(a => {
+                        {evento.atividades.length > 0 ?
+                            evento.atividades.map(a => {
                                 return <span key={a.id}>{a.nome}</span>
                             })
                             :
