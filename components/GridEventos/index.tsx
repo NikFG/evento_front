8179d8c1c26c7b4fe7dbbca19bfc9fed8ac892a8
Nikollas <nikollasferreirag@gmail.com>
2021-09-20 15:20:53 -0300

    feat: refaz GridEventos

--- components/GridEventos/index.tsx
+++ components/GridEventos/index.tsx
@@ -1,24 +1,32 @@
-import styles from "./GridEventos.module.css";
 import CardEvento from "@components/CardEvento";
-import {Evento} from "@types";
+import styles from "./GridEventos.module.css"
+import {Evento} from '@types';
+import React from "react";
+
 
 export interface EventoProps {
     eventos: Evento[]
 }
 
-export default function GridEventos(props: EventoProps) {
+export default function GridEventos({eventos}: EventoProps) {
+
     return (
-        <div className={"container-fluid " + styles.area}>
-            <h3>Veja os incríveis eventos mais próximos</h3>
+        <div className={"container-fluid"}>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-10">
+                        <div className="section-title text-center" style={{paddingBottom: "40px"}}>
+                            <div className="line m-auto"/>
+                            <h3 className={''}>Os maiores eventos você entra aqui!</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className={"row"}>
-                {props.eventos.map(e => (
-                    <div className={`col-md-4 col-lg-3 col-sm-1 ${styles.cartao}`} key={e.id}>
-                        <CardEvento id={e.id}
-                                    nome={e.nome}
-                                    categoria={e.categoria ? e.categoria.nome : ""}
-                                    tipo={e.tipo}
-                                    breve_descricao={e.breve_descricao}
-                                    instituicao={"123"}/>
+                {eventos.map(e => (
+                    <div className={`col-4 ${styles.cartao}`} key={e.id}>
+                        <CardEvento id={e.id ?? 0} nome={e.nome} descricao={e.breve_descricao}
+                                    data_inicio={e.atividades[0].data} instituicao={e.instituicao?.nome ?? ""}/>
                     </div>
                 ))}
             </div>
