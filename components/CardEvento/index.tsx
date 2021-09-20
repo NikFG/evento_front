9222fa28e56ebef103625f21b213b635e0666bd1
Nikollas <nikollasferreirag@gmail.com>
2021-09-20 15:20:39 -0300

    feat: refaz cardEvento

--- components/CardEvento/index.tsx
+++ components/CardEvento/index.tsx
@@ -1,35 +1,49 @@
-import styles from "./CardEvento.module.css";
+import styles from './CardEvento.module.css';
 import Image from "next/image";
-import {useRouter} from 'next/router';
+import {useRouter} from "next/router";
+import React from "react";
 
 export interface EventoProps {
-    id?: number,
+    id: number,
     nome: string,
-    breve_descricao: string,
+    descricao: string,
+    data_inicio: string,
     instituicao: string,
-    categoria?: string,
-    tipo: string,
+    // categoria?: string,
 }
 
-export default function CardEvento(props: EventoProps) {
+
+export default function CardEvento({id, nome, descricao, data_inicio, instituicao}: EventoProps) {
     const router = useRouter();
     return (
-        <div className={"card " + styles.divCard} onClick={() => {
-           router.push(`/eventos/${props.id}`)
+        <div className={"container"} onClick={() => {
+            router.push(`/eventos/${id}`)
         }}>
-            <div className={"card-header"}><h5 className="card-title">{props.nome}</h5></div>
-            <Image src={`https://via.placeholder.com/500x400/e66?text=${props.id}`} width={500} height={400}
-                   alt={"teste"}/>
-            <div className={""}>
-                <ul className={"list-group list-group-flush"}>
-                    <li className={"list-group-item"}><p className="card-text">{props.breve_descricao}</p></li>
-                    <li className={"list-group-item"}><p className={"card-text"}>{props.categoria} - {props.tipo}</p>
-                    </li>
-                </ul>
+            <div className="justify-content-center">
+                <div className={"col-lg-10 col-md-8 " + styles.eventos}>
+                    <div className={"mt-2"}>
+                        <div style={{margin: "5px"}}>
+                            <Image src={`https://via.placeholder.com/500x400/e66?text=${id}`} width={500}
+                                   height={400} alt={"placeholder"}/>
+                        </div>
+                    </div>
+                    <div style={{margin: "10px"}}>
+                        <h2>
+                            {nome}
+                        </h2>
+                        <hr/>
+                        <p className="mx-3">{descricao}</p>
+                        <hr/>
+                        <p className={"mx-3"}>
+                            <span>Evento por: {instituicao}</span>
+                        </p>
+                        <p className={"mx-3"}>
+                            Início: {data_inicio}
+                        </p>
+                    </div>
+                </div>
 
             </div>
-
-            <div className={"card-footer"}>Data: 23/08/2021</div>
         </div>
     );
-}
+};
