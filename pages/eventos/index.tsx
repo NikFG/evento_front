f25f68abb5e9d1d5401b12459902dc43d7f3af9f
Nikollas <nikollasferreirag@gmail.com>
2021-09-28 09:22:37 -0300

    feat: melhora página do index de eventos
    A melhoria consiste em adicionar os parâmetros de URL e também enviar a API para requirir apenas os eventos selecionados da pesquisa

--- pages/eventos/index.tsx
+++ pages/eventos/index.tsx
@@ -1,27 +1,49 @@
 import Navbar from "@components/Navbar";
-import {GetStaticProps, InferGetStaticPropsType} from "next";
-import {Evento} from "@types";
+import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import {Categoria, Evento, Instituicao} from "@types";
 import GridEventos from "@components/GridEventos";
+import axios from "axios";
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
     const axios = require('axios');
-    const res = await axios.get(process.env.API_SERVER + '/eventos');
+    const api = process.env.API_SERVER;
+    const urlQuery = context.query;
+    let query = '';
+    for (const [k, v] of Object.entries(urlQuery)) {
+        query += `${k}=${v}&`;
+    }
+    query = query.slice(0, -1);
+    let res = await axios.get(api + '/eventos?' + query);
 
     const eventos: Evento[] = res.data;
+
+
+
+    res = await axios.get(api + "/categorias");
+    const categorias: Categoria[] = res.data;
+
+    res = await axios.get(api + "/instituicao");
+    const instituicoes: Instituicao[] = res.data;
 
     return {
         props: {
             eventos,
+            categorias,
+            instituicoes,
 
         },
     }
 }
-export default function Eventos({eventos}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Eventos({
+                                    eventos,
+                                    categorias,
+                                    instituicoes
+                                }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
     return (
         <>
             <Navbar/>
-            <GridEventos eventos={eventos}/>
+            <GridEventos eventos={eventos} categorias={categorias} instituicoes={instituicoes}/>
         </>
     );
 }
