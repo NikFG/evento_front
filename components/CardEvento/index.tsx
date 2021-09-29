ffafdd26b131afd1f3c9819d4eb64e45e48831ad
Nikollas <nikollasferreirag@gmail.com>
2021-09-29 13:34:44 -0300

    feat: card mostra a imagem de banner vinda do banco

--- components/CardEvento/index.tsx
+++ components/CardEvento/index.tsx
@@ -2,18 +2,19 @@ import styles from './CardEvento.module.css';
 import Image from "next/image";
 import {useRouter} from "next/router";
 import React from "react";
-
+import blur from '@images/blur.png'
 export interface EventoProps {
     id: number,
     nome: string,
     descricao: string,
     data_inicio: string,
     instituicao: string,
-    // categoria?: string,
+    banner?: string
 }
 
 
-export default function CardEvento({id, nome, descricao, data_inicio, instituicao}: EventoProps) {
+export default function CardEvento({id, nome, descricao, data_inicio, instituicao, banner}: EventoProps) {
+
     const router = useRouter();
     return (
         <div className={"container"} onClick={() => {
@@ -23,8 +24,14 @@ export default function CardEvento({id, nome, descricao, data_inicio, instituica
                 <div className={"col-lg-10 col-md-8 " + styles.eventos}>
                     <div className={"mt-2"}>
                         <div style={{margin: "5px"}}>
-                            <Image src={`https://via.placeholder.com/500x400/e66?text=${id}`} width={500}
-                                   height={400} alt={"placeholder"}/>
+                            {!banner ?
+                                <Image src={`https://via.placeholder.com/500x400/e66?text=${id}`} width={500}
+                                       height={400} alt={"placeholder"}/> :
+                                <Image src={`data:image/jpeg;base64,${banner}`} width={500} height={400}
+                                       blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="}
+                                       alt={"placeholder"}
+                                />
+                            }
                         </div>
                     </div>
                     <div style={{margin: "10px"}}>
