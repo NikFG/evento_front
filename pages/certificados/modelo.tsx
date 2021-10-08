88e9bc1410f8fd4d5a50dec14fde30891e4befe3
Nikollas <nikollasferreirag@gmail.com>
2021-10-08 09:44:37 -0300

    feat: cria protótipo do componente de gerar certificado

---
+++ pages/certificados/modelo.tsx
@@ -0,0 +1,11 @@
+import ModeloCertificado from "@components/ModeloCertificado";
+import Navbar from "@components/Navbar";
+
+export default function ModeloCertificadoPage() {
+    return (
+        <>
+            <Navbar/>
+            <ModeloCertificado/>
+        </>
+    );
+}
