0fe039ce18c7adf84d279de9575ba3b42ea741d6
Nikollas <nikollasferreirag@gmail.com>
2021-10-22 10:05:01 -0300

    feat: passagem de certificados e url de api para os componentes

--- pages/usuario/index.tsx
+++ pages/usuario/index.tsx
@@ -9,36 +9,48 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const axios = require('axios');
     const cookies = parseCookies(context)
     const token = cookies.USER_TOKEN;
-    let res = await axios.get(process.env.API_SERVER + "/eventos/user", {
+    const api = process.env.API_SERVER;
+    let res = await axios.get(api + "/eventos/user", {
         headers: {
             Authorization: `Bearer ${token}`
         }
     });
     const eventos_participados: Evento[] = res.data;
-    res = await axios.get(process.env.API_SERVER + "/eventos/criados", {
+    res = await axios.get(api + "/eventos/criados", {
         headers: {
             Authorization: `Bearer ${token}`
         }
     });
     const eventos_criados: Evento[] = res.data;
-
+    res = await axios.get(api + "/certificados", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    const certificados: Certificado[] = res.data
     return {
         props: {
             eventos_participados,
             eventos_criados,
+            certificados,
+            api
         }
     }
 }
 
 export default function UsuarioPage({
                                         eventos_participados,
-                                        eventos_criados
+                                        eventos_criados, certificados, api
                                     }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
     return (
         <>
-            <Navbar/>
-            <Usuario eventos_criados={eventos_criados} eventos_participados={eventos_participados}/>
+            <Navbar api={api}/>
+            <Usuario
+                eventos_criados={eventos_criados}
+                eventos_participados={eventos_participados}
+                certificados={certificados}
+            />
 
         </>
     );
