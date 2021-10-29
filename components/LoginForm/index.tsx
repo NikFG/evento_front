84854a81e4cbd9e272ffda98d5a1fd5d62d3aa88
Nikollas <nikollasferreirag@gmail.com>
2021-10-29 14:44:20 -0300

    feat: cria toast para login

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -6,6 +6,7 @@ import {User} from "@types";
 import {encrypt} from "@utils";
 import nookies, {setCookie} from 'nookies';
 import React from "react";
+import {toast, ToastContainer} from "react-toastify";
 
 export interface LoginProps {
     secret: string
@@ -24,7 +25,7 @@ export default function LoginForm(props: LoginProps) {
             email,
             password
         }
-        await axios.post(`${props.api}/user/login`, user).then((value: AxiosResponse) => {
+        await axios.post(`${props.api}/user/login`, user).then(async (value: AxiosResponse) => {
             sessionStorage.setItem("USER_TOKEN", value.data.access_token)
             let usuario_criptografado = encrypt(user)
             const user_logado: User = value.data.user;
@@ -36,78 +37,110 @@ export default function LoginForm(props: LoginProps) {
                 sameSite: 'strict',
                 secure: true
 
-            })
-            router.push("/")
+            });
+            await toast.success(`Login realizado com sucesso!`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            await router.push("/");
         })
             .catch((error: any) => {
-                console.error(error)
+                console.error(error.response.data.error);
+                toast.error(`${error.response.data.error}`, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
             });
     }
 
     return (
-        <div className={styles.outer}>
-            <div className={styles.inner}>
-                <form method={"POST"} onSubmit={handleSubmit}>
-                    <h3>Log in</h3>
-                    <div className="form-group mb-3">
-                        <label htmlFor={"email"} className={"form-label"}>Email</label>
-                        <input id="email" type="email" className="form-control" placeholder="Digite seu email"
-                               value={email}
-                               onChange={(e) => {
-                                   setEmail(e.target.value)
-                               }}
-                               required
-                        />
-                    </div>
-
-                    <div className="form-group mb-3">
-                        <label htmlFor="senha" className={"form-label"}>Senha</label>
-                        <input id="senha" type="password" className="form-control" placeholder="Digite sua senha"
-                               value={password}
-                               onChange={(e) => {
-                                   setPassword(e.target.value)
-                               }}
-                               required
-                        />
-                        <p className="text-end">
-                            Esqueceu sua <a href="#">senha?</a>
-                        </p>
-                    </div>
-
-                    <div className="form-group">
-                        <div className="mb-3 form-check">
-                            <input type="checkbox" className="form-check-input" id="lembrar" name="lembrar"/>
-                            <label htmlFor="lembrar"
-                                   className={"custom-control-label " + styles.unselectable}>Lembrar</label>
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme={"colored"}
+                style={{width: "500px", maxWidth: "1000px", whiteSpace: "pre-line"}}/>
+            <div className={styles.outer}>
+                <div className={styles.inner}>
+                    <form method={"POST"} onSubmit={handleSubmit}>
+                        <h3>Log in</h3>
+                        <div className="form-group mb-3">
+                            <label htmlFor={"email"} className={"form-label"}>Email</label>
+                            <input id="email" type="email" className="form-control" placeholder="Digite seu email"
+                                   value={email}
+                                   onChange={(e) => {
+                                       setEmail(e.target.value)
+                                   }}
+                                   required
+                            />
                         </div>
-                    </div>
-                    <div className={"row form-group " + styles.botao}>
-                        <button type="submit" className="btn btn-outline-primary btn-lg btn-block">Entrar</button>
 
-
-                    </div>
-                    <div className={"row mt-2"}>
-                        <div className={"col-2"}>
-                            <p className={"text-start"}>
-                                <Link href={"/"}>
-                                    <a>Voltar</a>
-                                </Link>
-                            </p></div>
-
-                        <div className={"col-10"}>
-                            <p className={"text-end"}>
-                                <Link href={"/cadastro"}>
-
-                                    <a>Criar conta</a>
-
-                                </Link>
+                        <div className="form-group mb-3">
+                            <label htmlFor="senha" className={"form-label"}>Senha</label>
+                            <input id="senha" type="password" className="form-control" placeholder="Digite sua senha"
+                                   value={password}
+                                   onChange={(e) => {
+                                       setPassword(e.target.value)
+                                   }}
+                                   required
+                            />
+                            <p className="text-end">
+                                Esqueceu sua <a href="#">senha?</a>
                             </p>
                         </div>
-                    </div>
+
+                        <div className="form-group">
+                            <div className="mb-3 form-check">
+                                <input type="checkbox" className="form-check-input" id="lembrar" name="lembrar"/>
+                                <label htmlFor="lembrar"
+                                       className={"custom-control-label " + styles.unselectable}>Lembrar</label>
+                            </div>
+                        </div>
+                        <div className={"row form-group " + styles.botao}>
+                            <button type="submit" className="btn btn-outline-primary btn-lg btn-block">Entrar</button>
 
 
-                </form>
+                        </div>
+                        <div className={"row mt-2"}>
+                            <div className={"col-2"}>
+                                <p className={"text-start"}>
+                                    <Link href={"/"}>
+                                        <a>Voltar</a>
+                                    </Link>
+                                </p></div>
+
+                            <div className={"col-10"}>
+                                <p className={"text-end"}>
+                                    <Link href={"/cadastro"}>
+
+                                        <a>Criar conta</a>
+
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+
+
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
