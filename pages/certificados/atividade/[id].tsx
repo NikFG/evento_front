eb91439467a2b77a5797318e23f86a44b5549e4d
Nikollas <nikollasferreirag@gmail.com>
2021-11-25 15:44:54 -0300

    feat: modifica rotas para ficar de acordo com a API mais recente
    Foram modificadas pensando nos dados que agora pertencem a outras rotas

--- pages/certificados/atividade/[id].tsx
+++ pages/certificados/atividade/[id].tsx
@@ -1,7 +1,7 @@
 import Navbar from "@components/Navbar";
 import GeraCertificado from "@components/GeraCertificado";
 import {GetStaticPaths, GetStaticProps, InferGetStaticPropsType} from "next";
-import {Atividade} from "@types";
+import {Apresentador, Atividade, ModeloCertificado, User} from "@types";
 import {ParsedUrlQuery} from "querystring";
 
 interface IParams extends ParsedUrlQuery {
@@ -25,27 +25,35 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
     const {id} = context.params as IParams;
-    const axios = require('axios');
-    const res = await axios.get(process.env.API_SERVER + `/atividades/${id}`);
-    const atividade: Atividade = res.data;
     const api = process.env.API_SERVER;
+    const axios = require('axios');
+    let res = await axios.get(`${api}/atividades/participantes/${id}`);
+
+    const participantes: User[] = res.data.participantes;
+    const apresentadores: Apresentador[] = res.data.apresentadores;
+    const instituicao = res.data.instituicao;
+    res = await axios.get(`${api}/modelos/instituicao/${instituicao}`);
+    const modelos: ModeloCertificado = res.data;
     return {
         props: {
-            atividade,
-            api
+            participantes,
+            apresentadores,
+            api,
+            modelos
         },
         revalidate: 10
     }
 }
 
 
-export default function GeraCertificadoPage({atividade, api}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function GeraCertificadoPage({participantes,
+                                                apresentadores, api, modelos}: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
         <>
             <Navbar titulo={"Gerar certificado"}/>
             {
-                atividade.users?.length > 0 ?
-                    <GeraCertificado atividade={atividade} api={api}/> :
+                participantes.length > 0 ?
+                    <GeraCertificado participantes={participantes} apresentadores={apresentadores} api={api} modelos={modelos}/> :
                     <h2>Não há participantes para esta atividade</h2>
             }
 
