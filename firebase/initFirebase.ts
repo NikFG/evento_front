c91fc08b14fa68d134eb2d6d5c54722f9d170137
Nikollas <nikollasferreirag@gmail.com>
2022-03-15 19:42:25 -0300

    feat: remove do .env temporariamente variáveis firebase

--- firebase/initFirebase.ts
+++ firebase/initFirebase.ts
@@ -4,13 +4,13 @@ import 'firebase/storage';
 
 
 const clientCredentials = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDw-3H5VklzyDv4PYxpvuuLeB4t0wwko2w",
+    authDomain:"eventos-dados.firebaseapp.com",
+    projectId:'eventos-dados',
+    storageBucket: "eventos-dados.appspot.com",
+    messagingSenderId: "663153003337",
+    appId: "1:663153003337:web:1bc6dbadaae31413f66898",
+    measurementId: "G-JXFDJFCQVK"
 }
 
 
